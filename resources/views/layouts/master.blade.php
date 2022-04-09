2a338a7c5f33782dfbe75c9ec25a482a014fad96
rindrajosia <rrindrajosia@gmail.com>
2022-04-09 14:43:42 +0300

    Create components blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,39 +15,8 @@
 <body class="hold-transition sidebar-mini">
   <div class="wrapper">
 
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <x-topnav />
 
-
-      <ul class="navbar-nav ml-auto">
-
-        <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
-            <i class="far fa-bell"></i>
-            <span class="badge badge-warning navbar-badge">15</span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-            <span class="dropdown-header">15 Notifications</span>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-envelope mr-2"></i> 4 new messages
-              <span class="float-right text-muted text-sm">3 mins</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-            <i class="fas fa-expand-arrows-alt"></i>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-            <i class="fas fa-user"></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
 
 
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
@@ -67,44 +36,8 @@
           </div>
         </div>
 
+        <x-menu />
 
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
-            <li class="nav-item menu-open">
-              <a href="#" class="nav-link active">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>
-                  Starter Pages
-                  <i class="right fas fa-angle-left"></i>
-                </p>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="#" class="nav-link active">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Active Page</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a href="#" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Inactive Page</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
-                <p>
-                  Simple Link
-                  <span class="right badge badge-danger">New</span>
-                </p>
-              </a>
-            </li>
-          </ul>
-        </nav>
 
       </div>
 
@@ -128,46 +61,7 @@
 
     </div>
 
-
-    <aside class="control-sidebar control-sidebar-dark">
-
-      <div class="p-1">
-        <div class="bg-dark">
-          <div class="card-body box-profile">
-            <div class="text-center">
-              <img class="profile-user-img img-fluid img-circle" src="{{asset('images/user.jpg')}}" alt="User profile picture">
-            </div>
-
-            <h3 class="profile-username text-center ellipsis">{{ userFullName() }}</h3>
-            <p class="text-muted text-center">Software Engineer</p>
-
-
-            <ul class="list-group bg-dark mb-3">
-              <li class="list-group-item">
-                <a href="#" class="d-flex align-items-center "><i class="fa fa-lock pr-2"></i><b >Password</b> </a>
-              </li>
-              <li class="list-group-item">
-                <a href="#" class="d-flex align-items-center"><i class="fa fa-user pr-2"></i><b >Profile</b> </a>
-              </li>
-            </ul>
-
-
-            <a class="btn btn-primary btn-block" href="{{ route('logout') }}"
-               onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-
-
-          </div>
-
-        </div>
-      </div>
-    </aside>
+    <x-sidebar />
 
 
     <footer class="main-footer">
@@ -180,7 +74,7 @@
     </footer>
   </div>
 
-  <script src="{{mix("js/app.js")}}"></script>
+  <!-- <script src="{{mix("js/app.js")}}"></script> -->
   @livewireScripts
 </body>
 </html>
